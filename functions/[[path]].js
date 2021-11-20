67cc0d7eb8c53c7504e62c9992b705b93936dbfd
Matteo Rigon <matteo.rigon7@gmail.com>
2021-11-20 13:23:08 +0100

    feat: add catch-all function

---
+++ functions/[[path]].js
@@ -0,0 +1,86 @@
+// TODO hardcoding the relative location makes this brittle
+import { init, render } from '../.svelte-kit/output/server/app.js';
+import { getAssetFromKV, NotFoundError } from '@cloudflare/kv-asset-handler';
+
+init();
+
+addEventListener('fetch', (event) => {
+	event.respondWith(handle(event));
+});
+
+async function handle(event) {
+	// try static files first
+	const response = await getFromKV(event);
+	if (response) {
+		return response;
+	}
+
+	// fall back to dynamic rendering
+	const request = event.request;
+	const request_url = new URL(request.url);
+
+	try {
+		const rendered = await render({
+			host: request_url.host,
+			path: request_url.pathname,
+			query: request_url.searchParams,
+			rawBody: await read(request),
+			headers: Object.fromEntries(request.headers),
+			method: request.method
+		});
+
+		if (rendered) {
+			
+
+			return new Response(rendered.body, {
+				status: rendered.status,
+				headers: makeHeaders(rendered.headers)
+			});
+		}
+	} catch (e) {
+		return new Response('Error rendering route:' + (e.message || e.toString()), { status: 500 });
+	}
+
+	return new Response({
+		status: 404,
+		statusText: 'Not Found'
+	});
+}
+
+/** @param {Request} request */
+async function read(request) {
+	return new Uint8Array(await request.arrayBuffer());
+}
+
+/**
+ * @param {Record<string, string | string[]>} headers
+ * @returns {Request}
+ */
+function makeHeaders(headers) {
+	const result = new Headers();
+	for (const header in headers) {
+		const value = headers[header];
+		if (typeof value === 'string') {
+			result.set(header, value);
+			continue;
+		}
+		for (const sub of value) {
+			result.append(header, sub);
+		}
+	}
+	return result;
+}
+
+async function getFromKV(event) {
+	if (event.request.method == 'GET') {
+		try {
+			return await getAssetFromKV(event);
+		} catch (e) {
+			if (!(e instanceof NotFoundError)) {
+				return new Response('Error loading static asset:' + (e.message || e.toString()), {
+					status: 500
+				});
+			}
+		}
+	}
+}
